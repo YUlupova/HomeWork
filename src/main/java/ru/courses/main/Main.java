c50352315bf77e32bd097f0b684e8ed35aadecc8
Yuliya Ulupova <git.user>
2023-10-22 20:22:20 +0300

    В классе Main создан метод, принимающий две строки, в которых будут записаны числа X и Y. Возвращает метод результат возведения X в степень Y.

--- src/main/java/ru/courses/main/Main.java
+++ src/main/java/ru/courses/main/Main.java
@@ -1,7 +1,19 @@
 package ru.courses.main;
 
-public class Main {
-    public static void main(String[] args) {
+import static java.lang.Integer.parseInt;
+import static java.lang.Math.pow;
 
+public class Main {
+
+    public static int power(String x, String y) {
+        int num1 = parseInt(x);
+        int num2 = parseInt(y);
+        return (int) pow(num1, num2);
+    }
+
+    public static void main(String[] args) {
+        String x = "2";
+        String y = "3";
+        System.out.println(power(x, y));
     }
 }
